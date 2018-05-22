5fe9319071c1e7ec8d135641ffc2fb93294da5f0
DrAllStar16 <league@imac.attlocal.net>
2018-05-21 20:01:44 -0700

    CirlceRing Code4Life EvilPolygons

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -6,29 +7,39 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot r2d2 = new Robot();
+		// 2. Set your robot’s position to x=150 and y=200
+		r2d2.setX(150);
+		r2d2.setY(200);
+		// 3. Put the robot's pen down
+		r2d2.penDown();
+		// 4. Set the robot’s speed to 10
+		r2d2.setSpeed(10);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			r2d2.move(3);
+			r2d2.turn(1);
 
+			if (i % 20 == 0) {
+				for (int j = 0; j < 360; j++) {
+					r2d2.move(1);
+					r2d2.turn(1);
+				}
+			}
+			// 6. Move the robot 3 pixels
 
-       //  2. Set your robot’s position to x=150 and y=200
+			// 7. Turn the robot 1 degree
 
-        // 3. Put the robot's pen down
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
-        // 4. Set the robot’s speed to 10
+			// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-        // 5. Do everything below here 360 times (use i as the counter)
+			// 10. Move the robot 1 pixel
 
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
-}
+			// 11. Turn the robot 1 degree
+		}
+	}
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -9,20 +9,37 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot r2d2 = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+r2d2.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Choose a Color", "Evil Polygons", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Green", "Blue"}, 0);
+		System.out.println(colorChoice);
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
+		if(colorChoice == 2) {
+			r2d2.setPenColor(Color.blue);
+		}
+		else if (colorChoice ==1) {
+			r2d2.setPenColor(Color.green);
+		}
+		else {
+			r2d2.setPenColor(Color.red);
+		}
 		
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String poly = JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		int x = Integer.parseInt(poly);
+		for (int y = 0; y < x; y++) {
+			r2d2.move(30);
+			r2d2.turn(360/5);
+			
+				
+			}
+	
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
 	}
-}
+
 
