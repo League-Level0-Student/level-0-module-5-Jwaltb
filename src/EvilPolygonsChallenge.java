0f1f0df84ae722106deef645a673f92588f95b93
league <league@wts-im-17.attlocal.net>
2018-05-28 19:37:02 -0700

    evilpolygons goofynames

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -5,41 +5,44 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
-Robot r2d2 = new Robot();
-		//2. Set the speed to 100
-r2d2.setSpeed(100);
-		int colorChoice=JOptionPane.showOptionDialog(null, "Choose a Color", "Evil Polygons", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Green", "Blue"}, 0);
+
+		// 1. Create a new Robot
+		Robot r2d2 = new Robot("mini");
+		// 2. Set the speed to 100
+		r2d2.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "Choose a Color", "Evil Polygons",
+				JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null,
+				new String[] { "Red", "Green", "Blue" }, 0);
 		System.out.println(colorChoice);
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		if(colorChoice == 2) {
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		if (colorChoice == 2) {
 			r2d2.setPenColor(Color.blue);
-		}
-		else if (colorChoice ==1) {
+			r2d2.penDown();
+		} else if (colorChoice == 1) {
 			r2d2.setPenColor(Color.green);
-		}
-		else {
+			r2d2.penDown();
+		} else {
 			r2d2.setPenColor(Color.red);
+			r2d2.penDown();
 		}
-		
-		//4. Ask the use how many polygons they want to be drawn.
+
+		// 4. Ask the use how many polygons they want to be drawn.
 		String poly = JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
-		//5. Use the robot to draw the number of polygons the user requested.
+		// 5. Use the robot to draw the number of polygons the user requested.
 		int x = Integer.parseInt(poly);
 		for (int y = 0; y < x; y++) {
-			r2d2.move(30);
-			r2d2.turn(360/5);
-			
-				
+			for (int i = 0; i < 5; i++) {
+				r2d2.move(30);
+				r2d2.turn(360 / 5);
 			}
-	
+			r2d2.move(5);
 		}
-		//6. Make it so your shapes do not overlap
 
-		//7. Challenge: add more colors to the Option Dialog.
 	}
+	// 6. Make it so your shapes do not overlap
 
-
+	// 7. Challenge: add more colors to the Option Dialog.
+}
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -7,9 +7,18 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
-
-		// 1. Ask the user to enter their name
+		String goofyName = JOptionPane.showInputDialog("What is your name?");
+		String output = "";
+System.out.println(goofyName.toUpperCase());
+for (int i = 0; i < goofyName.length(); i++) {
+	if (i%2==0) {
+		output += goofyName.substring(i, i+1).toUpperCase();
+			}
+	if(i%2==1) {
+		output += goofyName.substring(i, i+1).toLowerCase();
+	}}
+JOptionPane.showMessageDialog(null, output);
+// 1. Ask the user to enter their name
 
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
